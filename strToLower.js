254e74dc2cd02a6064d3f3012caafd18b3a277be
DEV <connortodd0304@gmail.com>
2020-03-24 13:46:29 +0000

    group-3 dojo solution

--- strToLower.js
+++ strToLower.js
@@ -1,5 +1,45 @@
-function strToLower(str) {
+// function strToLower(str) {
     //Your code here
-}
+//     return str.toLowerCase();
+// }
+
+// const sentence='hello'
+// console.log(strToLower(sentence));
+
+// function strLowerCase(str) {
+//     let newStr = "";
+//      for(let i = 0; i < str.length; i++) {
+//      newStr = charCodeAt(str, i) + newStr;
+//      return newStr;
+//      }
+//  }
+
+
+// const result=''
+
+//     for ( let i = 0; i < str.length; i ++) {
+//             str.substring(i, i+1).toLowerCase()
+//             result=result+ str.substring(i, i+1).toLowerCase()
+//         }
+//     }
+
+
+// console.log('a');
+
+
+m
+
+
+arnaud ca marche comme ca
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const index = 4;
+
+console.log('The character code ' + sentence.charCodeAt(index) + ' is equal to ' + sentence.charAt(index));
+// expected output: "The character code 113 is equal to q"
+
+// ?? j'ai rien qui sont 
+lol on peut jump sur un link?
+
 
 module.exports = strToLower;
