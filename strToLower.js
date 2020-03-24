4c121fe19ffdaed33889087a7a01118159fe0f26
DEV <connortodd0304@gmail.com>
2020-03-24 10:44:36 +0000

    group 1 solution

--- strToLower.js
+++ strToLower.js
@@ -1,5 +1,36 @@
-function strToLower(str) {
-    //Your code here
-}
+//function strToLower(str) {
+    //const newString = str.toLowerCase();
+    //return newString;
+//}
+
+//function strToLower(str) {
+    //let array = [];
+
+    //for (let i = 0; i < str.length; i++){
+        //let newString = str.charCodeAt(i);
+        //array.push(newString);
+        //console.log(newString);
+        //return newString;
+    
+
+    function strToLower(str) {
+        let newStr = "";
+        for(let i = 0; i < str.length; i++) {
+            let code = str.charCodeAt(i);
+            console.log(code);
+            if(code >= 65 && code <= 90) {
+                code += 32;
+            } newStr += String.fromCharCode(code);
+            
+        }
+        console.log(newStr);
+        return newStr;
+    }
+    
+    
+
+
+
+strToLower("HELLo WORLD");
 
 module.exports = strToLower;
